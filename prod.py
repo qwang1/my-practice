88dac55b40228b24de46fd072b8d1fdee9e9c153
Qixuan Wang <qwang@redhat.com>
2018-01-03 13:58:13 +0800

    Update prod.py

--- prod.py
+++ prod.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()>求积
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积
 
 from functools import reduce
 
